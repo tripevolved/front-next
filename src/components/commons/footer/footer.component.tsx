1729170f9a132f050da8f1b397e9d9a8ab656333
PGabriel-MB <pablogrbarbosa@gmail.com>
2022-12-08 18:07:53 -0300

    finalizado os itens do footer

--- src/components/commons/footer/footer.component.tsx
+++ src/components/commons/footer/footer.component.tsx
@@ -3,9 +3,13 @@ import { BrandLettering } from "@/ui/icons/brand";
 import { Instagram } from "@/ui/icons/instagram";
 import { YouTube } from "@/ui/icons/youtube";
 import { Link } from "@/ui/link";
+import { Picture } from "@/ui/picture";
 import { Section } from "@/ui/section";
 import { Text } from "@/ui/text";
 import { Box, Flex } from "@chakra-ui/react";
+
+import AbavLogo from "@/public/assets/home/AbavLogo.png";
+import CadasturLogo from "@/public/assets/home/CadasturLogo.png";
 
 export const Footer = () => {
   return (
@@ -52,6 +56,10 @@ export const Footer = () => {
               <YouTube />
             </Link>
           </Flex>
+        </Box>
+        <Box pt={4} display="flex" justifyContent="center" flexDirection="column">
+          <Picture centered src={CadasturLogo} width={385} height={5} />
+          <Picture centered src={AbavLogo} width={225} height={27} mt={5} />
         </Box>
       </Flex>
     </Section>
