4cbf9ae4f2ead0c8fae620f2c92037cc06852a11
PGabriel-MB <pablogrbarbosa@gmail.com>
2022-12-13 07:15:48 -0300

    arquivos iniciais

---
+++ src/pages/nossas-ideias.tsx
@@ -0,0 +1,13 @@
+import { newRenderComponentList } from "@/components/hoc/render-component-list";
+
+const componentList = {};
+
+const RenderComponent = newRenderComponentList(componentList);
+
+const HowWeWorks = () => {
+  return (
+    <main></main>
+  )
+};
+
+export default HowWeWorks;
