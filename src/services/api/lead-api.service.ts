18a528ebc6b332ed4abbe33d4bc59a5c44a00b04
fredes <ricardofredes@gmail.com>
2023-03-23 08:59:04 -0300

    feat(lead): send data to api

--- src/services/api/lead-api.service.ts
+++ src/services/api/lead-api.service.ts
@@ -1,6 +1,6 @@
 import { sendFormData } from "@/helpers/form.helpers";
-// import { ensureNotSlashEnds } from "@/helpers/url.helper";
-// import axios from "axios";
+import { ensureNotSlashEnds } from "@/helpers/url.helper";
+import axios from "axios";
 
 export interface Lead {
   name: string;
@@ -9,14 +9,16 @@ export interface Lead {
 }
 
 const ACTION_URL = "https://getlaunchlist.com/s/0l3TDN";
-// const API_URL = ensureNotSlashEnds(process.env.NEXT_PUBLIC_API_URL || "");
-// const url = `${API_URL}/api/create`
+const API_URL = ensureNotSlashEnds(process.env.NEXT_PUBLIC_API_URL || "");
+const url = `${API_URL}/api/customers/create`;
 
-const create = async (lead: Lead) => {
+const create = async ({ name, email, phone }: Lead) => {
+  const parsedPhone = `+55${phone.replace(/\D/g, "")}`;
+  const lead = { name, email, phone: parsedPhone };
   return Promise.all([
     sendFormData<Lead>(ACTION_URL, lead),
-    // axios.post(url, lead),
+    axios.post(url, lead),
   ]);
-}
+};
 
 export const LeadApiService = { create };
