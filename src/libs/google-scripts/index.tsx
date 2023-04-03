ae889e667edff03c685ac9a45af1d6e1371d20de
fredes <ricardofredes@gmail.com>
2023-04-03 20:09:56 -0300

    feat(google): add GA id

--- src/libs/google-scripts/index.tsx
+++ src/libs/google-scripts/index.tsx
@@ -1,10 +1,12 @@
 import { RawScript } from "../RawScript";
 
-const GTM_ID = process.env["NEXT_PUBLIC_GOOGLE_TAGMANAGER_ID"];
+const GTM = process.env["NEXT_PUBLIC_GTM"] || "GTM-KK9FMT3";
+const GA = process.env["NEXT_PUBLIC_GA"] || "G-4W2CG7W3GC";
 
 const GoogleScripts = () => (
   <>
-    <GtmScript id={GTM_ID} />
+    <script async src={`https://www.googletagmanager.com/gtm.js?id=${GTM}`} />
+    <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA}`} />
 
     <RawScript>{`
       (function(w,l){
@@ -13,19 +15,11 @@ const GoogleScripts = () => (
           'gtm.start':new Date().getTime(),
           event:'gtm.js',
         });
+        w[l].push('js', new Date())
+        w[l].push('config', ${GA})
       })(window,'dataLayer');
     `}</RawScript>
   </>
 );
-
-interface ScriptID {
-  url?: string;
-  id?: string;
-}
-
-function GtmScript({ id = "GTM-KK9FMT3" }: ScriptID) {
-  if (!id) return null;
-  return <script async src={`https://www.googletagmanager.com/gtm.js?id=${id}`} />;
-}
 
 export default GoogleScripts;
