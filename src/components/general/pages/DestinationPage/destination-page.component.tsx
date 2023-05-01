7b00903e837e4b873b33d846243f7b760987d5ab
Lucas de Oliveira <lucasmo@outlook.com.br>
2023-05-01 13:23:17 -0300

    feat: add faq section

--- src/components/general/pages/DestinationPage/destination-page.component.tsx
+++ src/components/general/pages/DestinationPage/destination-page.component.tsx
@@ -5,6 +5,37 @@ import { DestinationInfoSection } from "./destination-info.section";
 import { DestinationVideoSection } from "./destination-video.section";
 import { DestinationTipsSection } from "./destination-tips.section";
 import { DestinationPostsSection } from "./destinations-posts.section";
+import { DestinationFaqSection } from "./destination-faq.section";
+
+const mock = {
+  faq: [
+    {
+      question: "Passagem aérea",
+      answer:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+    {
+      question: "Hospedagem",
+      answer:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+    {
+      question: "Roteiro completo",
+      answer:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+    {
+      question: "Dicas gastronômicas",
+      answer:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+    {
+      question: "Suporte durante a viagem",
+      answer:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+  ],
+};
 
 
 export function DestinationPage({ destination, seo, navbar, footer }: DestinationPageProps) {
@@ -18,6 +49,7 @@ export function DestinationPage({ destination, seo, navbar, footer }: Destinatio
       <DestinationVideoSection videos={destination.videos} />
       <DestinationTipsSection tips={destination.tips} />
       <DestinationPostsSection posts={destination.posts} />
+      <DestinationFaqSection faq={mock.faq} title={destination.title} />
     </PageBase>
   );
 }
