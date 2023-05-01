b8c703bd0aa817001936d195c5d8430b59f92d34
Lucas de Oliveira <lucasmo@outlook.com.br>
2023-05-01 13:34:23 -0300

    feat: add helper toLowerCase

--- src/helpers/strings.helper.ts
+++ src/helpers/strings.helper.ts
@@ -1,3 +1,7 @@
 export const toCamelCase = (str: string) => {
   return str.replace(/[-_](\w)/g, (m, c) => c.toUpperCase());
 };
+
+export const toLowerCase = (str: string) => {
+  return str.toLowerCase().replace(/[ ](\w)/g, (m, c) => c);
+};
