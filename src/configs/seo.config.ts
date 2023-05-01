226e08886c64df99f14680d797a03e883b0b5ccb
fredes <ricardofredes@gmail.com>
2023-05-01 10:28:22 -0300

    feat: add photos

--- src/configs/seo.config.ts
+++ src/configs/seo.config.ts
@@ -3,6 +3,6 @@ export const seo = {
   description:
     "Tecnologia e curadoria especializada para uma experiência de viagem única e exclusiva em um sistema por assinatura de recomendações de destinos e roteiros de viagens.",
   url: "https://www.tripevolved.com.br/",
-  image: "/brand/thumbnail.png",
+  image: "https://images.prismic.io/tripevolved/77af5f9a-fa90-4736-acc4-0a632a96129e_Default.png?auto=compress,format",
   themeColor: "#0ab9ad",
 };
