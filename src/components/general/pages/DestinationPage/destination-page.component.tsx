0c9ae5639341cdd7116ed1bd3270805ba2e72407
Lucas de Oliveira <lucasmo@outlook.com.br>
2023-05-01 13:26:09 -0300

    feat: add assets hardcoded

--- src/components/general/pages/DestinationPage/destination-page.component.tsx
+++ src/components/general/pages/DestinationPage/destination-page.component.tsx
@@ -11,26 +11,31 @@ const mock = {
   faq: [
     {
       question: "Passagem aérea",
+      icon: "/assets/destino/passagem-aerea.svg",
       answer:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     },
     {
       question: "Hospedagem",
+      icon: "/assets/destino/hospedagem.svg",
       answer:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     },
     {
       question: "Roteiro completo",
+      icon: "/assets/destino/roteiro.svg",
       answer:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     },
     {
       question: "Dicas gastronômicas",
+      icon: "/assets/destino/dicas-gastronomicas.svg",
       answer:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     },
     {
       question: "Suporte durante a viagem",
+      icon: "/assets/destino/suporte.svg",
       answer:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     },
