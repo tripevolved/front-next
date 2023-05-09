747caf68a9929608de8245817bea0de600912e1b
Lucas de Oliveira <lucasmo@outlook.com.br>
2023-05-08 23:08:43 -0300

    feat: import components

--- src/components/internal/index.ts
+++ src/components/internal/index.ts
@@ -3,7 +3,7 @@
  * run "yarn component:index" to update
  */
 export * from "./AdaptedRibo";
-export * from "./NoSSR";
 export * from "./admin/AdminPage";
 export * from "./admin/Playground";
 export * from "./admin/SignIn";
+export * from "./NoSSR";
