1ff57bdafbb3ba658ca99006f207f7d24765d0fd
Lucas de Oliveira <lucasmo@outlook.com.br>
2023-05-08 23:04:50 -0300

    feat: add buttom component

---
+++ src/components/general/buttons/Button/button.types.ts
@@ -0,0 +1,8 @@
+import type { ButtonProps as MarsButtonProps } from "mars-ds";
+
+interface ButtonProps extends MarsButtonProps {
+  backgroundColor?: string;
+  color?: string;
+}
+
+export type { ButtonProps };
