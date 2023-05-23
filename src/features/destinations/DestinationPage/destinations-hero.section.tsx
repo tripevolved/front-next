75a194b77e150012c00e8edea2f2e3865cb83f48
fredes <ricardofredes@gmail.com>
2023-05-22 21:53:27 -0300

    feat(destination-hero): add photo

--- src/features/destinations/DestinationPage/destinations-hero.section.tsx
+++ src/features/destinations/DestinationPage/destinations-hero.section.tsx
@@ -1,14 +1,20 @@
+import { Photo } from "@/core/types";
 import type { DestinationProps } from "./destination-page.types";
 
-import { Text } from "@/ui";
+import { Picture, Text } from "@/ui";
 import { Container } from "mars-ds";
 
 interface DestinationHeroSectionProps extends Pick<DestinationProps, "title" | "photos"> {}
 
-export const DestinationHeroSection = ({ title, photos }: DestinationHeroSectionProps) => {
+export const DestinationHeroSection = ({ title, photos = [] }: DestinationHeroSectionProps) => {
+  // TODO: Implements carousel
+  const [photo] = photos;
+  const cover = photo ? parsePhoto(photo) : undefined;
+
   return (
     <section className="destination-hero-section">
       <div className="destination-hero-section__photos">
+        <Picture>{cover}</Picture>
       </div>
       <Container className="destination-hero-section__content">
         <Text heading as="h1" size="lg">
@@ -18,3 +24,18 @@ export const DestinationHeroSection = ({ title, photos }: DestinationHeroSection
     </section>
   );
 };
+
+const parseType = (type: string) => {
+  return {
+    md: "base",
+    lg: "sm",
+    xl: "md",
+    xxl: "lg"
+  }[type] || "lg"
+}
+
+const parsePhoto = ({ sources = [] }: Photo) =>
+  sources.reduce(
+    (acc, { type, url, ...props }) => ({ ...acc, [parseType(type)]: { src: url, ...props } }),
+    {}
+  );
