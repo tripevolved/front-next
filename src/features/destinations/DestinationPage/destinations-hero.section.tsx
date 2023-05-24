7026bb5592f55817eca7d021fc69c2bd5930e63b
fredes <ricardofredes@gmail.com>
2023-05-23 21:51:04 -0300

    feat: change height by no photo

--- src/features/destinations/DestinationPage/destinations-hero.section.tsx
+++ src/features/destinations/DestinationPage/destinations-hero.section.tsx
@@ -3,6 +3,7 @@ import type { DestinationProps } from "./destination-page.types";
 
 import { Picture, Text } from "@/ui";
 import { Container } from "mars-ds";
+import { makeCn } from "@/utils/helpers/css.helpers";
 
 interface DestinationHeroSectionProps extends Pick<DestinationProps, "title" | "photos"> {}
 
@@ -11,11 +12,17 @@ export const DestinationHeroSection = ({ title, photos = [] }: DestinationHeroSe
   const [photo] = photos;
   const cover = photo ? parsePhoto(photo) : undefined;
 
+  const cn = makeCn("destination-hero-section", {
+    "destination-hero-section--no-photo": !cover,
+  })();
+
   return (
-    <section className="destination-hero-section">
-      <div className="destination-hero-section__photos">
-        <Picture>{cover}</Picture>
-      </div>
+    <section className={cn}>
+      {cover ? (
+        <div className="destination-hero-section__photos">
+          <Picture>{cover}</Picture>
+        </div>
+      ) : null}
       <Container className="destination-hero-section__content">
         <Text heading as="h1" size="lg">
           <strong>{title}</strong>
@@ -26,13 +33,15 @@ export const DestinationHeroSection = ({ title, photos = [] }: DestinationHeroSe
 };
 
 const parseType = (type: string) => {
-  return {
-    md: "base",
-    lg: "sm",
-    xl: "md",
-    xxl: "lg"
-  }[type] || "lg"
-}
+  return (
+    {
+      md: "base",
+      lg: "sm",
+      xl: "md",
+      xxl: "lg",
+    }[type] || "lg"
+  );
+};
 
 const parsePhoto = ({ sources = [] }: Photo) =>
   sources.reduce(
