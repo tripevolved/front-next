4db6c8a151dc4959251234a326d0dc513c675a86
fredes <ricardofredes@gmail.com>
2023-06-18 17:25:30 -0300

    refactor: move usePasswordConfirmationToken to new file

--- src/features/auth/AuthPasswordConfirmationPage/auth-password-confirmation.component.tsx
+++ src/features/auth/AuthPasswordConfirmationPage/auth-password-confirmation.component.tsx
@@ -1,11 +1,9 @@
-import { useRouter } from "next/router";
 import { AuthPasswordConfirmationForm } from "./auth-password-confirmation.form";
-import { useEffect, useState } from "react";
 import { EmptyState, GlobalLoader } from "@/ui";
-import { delay } from "@/utils/helpers/delay.helpers";
 import { Button, Card } from "mars-ds";
 import { AuthSection } from "../AuthSection";
 import { AuthPasswordConfirmationNew } from "./auth-password-confirmation-new";
+import { usePasswordConfirmationToken } from "./use-password-confirmation.hook";
 
 export function AuthPasswordConfirmation() {
   const { isLoading, uniqueId, isExpired } = usePasswordConfirmationToken();
@@ -30,31 +28,3 @@ export function AuthPasswordConfirmation() {
   return <AuthPasswordConfirmationForm />;
 }
 
-const usePasswordConfirmationToken = () => {
-  const [isExpired, setIsExpired] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
-
-  const router = useRouter();
-  const queryId = router.query["unique-id"];
-  const uniqueId = typeof queryId === "string" ? queryId : undefined;
-
-  const getDataByUniqueId = async () => {
-    setIsLoading(true);
-    await delay(2000);
-
-    // TODO: implements logic here
-    const response = {};
-    setIsExpired(false);
-    setIsLoading(false);
-  };
-
-  useEffect(() => {
-    if (uniqueId) getDataByUniqueId();
-  }, [uniqueId]);
-
-  return {
-    isExpired,
-    isLoading,
-    uniqueId,
-  };
-};
---
+++ src/features/auth/AuthPasswordConfirmationPage/use-password-confirmation.hook.tsx
@@ -0,0 +1,32 @@
+import { delay } from "@/utils/helpers/delay.helpers";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+export const usePasswordConfirmationToken = () => {
+  const [isExpired, setIsExpired] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const router = useRouter();
+  const queryId = router.query["unique-id"];
+  const uniqueId = typeof queryId === "string" ? queryId : undefined;
+
+  const getDataByUniqueId = async () => {
+    setIsLoading(true);
+    await delay(2000);
+
+    // TODO: implements logic here
+    const response = {};
+    setIsExpired(false);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    if (uniqueId) getDataByUniqueId();
+  }, [uniqueId]);
+
+  return {
+    isExpired,
+    isLoading,
+    uniqueId,
+  };
+};
