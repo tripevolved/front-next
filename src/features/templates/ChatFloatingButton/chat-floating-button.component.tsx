a2b9bb3b18601cb5cb93384a3ff1f9857918ff8f
fredes <ricardofredes@gmail.com>
2023-07-03 16:21:54 -0300

    feat(chat-floating-button): add custom message

--- src/features/templates/ChatFloatingButton/chat-floating-button.component.tsx
+++ src/features/templates/ChatFloatingButton/chat-floating-button.component.tsx
@@ -1,8 +1,11 @@
 import { getWhatsappLink } from "@/utils/helpers/whatsapp.helpers";
 import { Icon } from "mars-ds";
 
-// const defaultMessage = "Olá, eu gostaria de planejar a minha próxima viagem!";
-const currentMessage = "Olá! Gostaria de saber mais sobre o cartão presente para o Dia dos Pais."
+const defaultMessage = "Olá, eu gostaria de planejar a minha próxima viagem!";
+const fathersDayMessage =
+  "Olá! Gostaria de saber mais sobre o cartão presente para o Dia dos Pais.";
+
+const currentMessage = process.env.MKT_FLAG === "fathers_day" ? fathersDayMessage : defaultMessage;
 const whatsappLink = getWhatsappLink(currentMessage);
 
 export function ChatFloatingButton() {
