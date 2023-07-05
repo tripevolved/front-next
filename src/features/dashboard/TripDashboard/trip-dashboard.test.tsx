a0b6140f8d48028ce07ab977934095acc95195e9
PGabriel-MB <pablogrbarbosa@gmail.com>
2023-07-05 20:44:00 -0300

    finished dashboard painel

--- src/features/dashboard/TripDashboard/trip-dashboard.test.tsx
+++ src/features/dashboard/TripDashboard/trip-dashboard.test.tsx
@@ -1,8 +1,18 @@
 import type { TripDashboardProps } from "./trip-dashboard.types";
 import { render } from "@testing-library/react";
 import { TripDashboard } from "./trip-dashboard.component";
+import { TripDashboard as iTripDashboard } from "@/core/types";
 
-const makeSut = (props?: TripDashboardProps) => render(<TripDashboard {...props} />);
+const mockDashboard: iTripDashboard = {
+  pedingActions: 0,
+  attractionsNumber: 3,
+  documents: 2,
+  flightAndTickets: 4,
+  tips: 9,
+};
+
+const makeSut = (props?: TripDashboardProps) =>
+  render(<TripDashboard {...props} tripDashboard={mockDashboard} />);
 
 describe("<TripDashboard>", () => {
   it("should render component", () => {
