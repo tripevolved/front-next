d56fdce8b1a884c29a9a173a1ed548bfa63a50d9
PGabriel-MB <pablogrbarbosa@gmail.com>
2023-07-11 11:48:12 -0300

    mocking useRouter on teste

--- src/features/dashboard/TripPendings/trip-pendings.test.tsx
+++ src/features/dashboard/TripPendings/trip-pendings.test.tsx
@@ -1,7 +1,9 @@
 import type { TripPendingsProps } from "./trip-pendings.types";
 import { render } from "@testing-library/react";
 import { TripPendings } from "./trip-pendings.component";
+import { mockUseRouter } from "@/utils/mocks/next-router.mock";
 
+mockUseRouter();
 const makeSut = (props?: TripPendingsProps) => render(<TripPendings {...props} />);
 
 describe("<TripPendings>", () => {
