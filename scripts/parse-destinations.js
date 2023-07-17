167838fdc25ac32cc9fbc14bf64985139bf067e3
fredes <ricardofredes@gmail.com>
2023-07-17 17:59:56 -0300

    chore: improve parse destination script

--- scripts/parse-destinations.js
+++ scripts/parse-destinations.js
@@ -1,51 +1,26 @@
-const data = [
-  {
-    image: {
-      dimensions: {
-        width: 1920,
-        height: 1080,
-      },
-      alt: "vitoria",
-      copyright: null,
-      url: "https://images.prismic.io/tripevolved/01baf97e-59ad-4947-831a-3cfd32ab8c7e_download.jpeg?auto=compress,format&rect=0,90,2880,1620&w=1920&h=1080",
-      md: {
-        dimensions: {
-          width: 600,
-          height: 600,
-        },
-        alt: null,
-        copyright: null,
-        url: "https://images.prismic.io/tripevolved/01baf97e-59ad-4947-831a-3cfd32ab8c7e_download.jpeg?auto=compress,format&rect=540,0,1800,1800&w=600&h=600",
-      },
-      lg: {
-        dimensions: {
-          width: 800,
-          height: 600,
-        },
-        alt: null,
-        copyright: null,
-        url: "https://images.prismic.io/tripevolved/01baf97e-59ad-4947-831a-3cfd32ab8c7e_download.jpeg?auto=compress,format&rect=240,0,2400,1800&w=800&h=600",
-      },
-      xl: {
-        dimensions: {
-          width: 1400,
-          height: 800,
-        },
-        alt: null,
-        copyright: null,
-        url: "https://images.prismic.io/tripevolved/01baf97e-59ad-4947-831a-3cfd32ab8c7e_download.jpeg?auto=compress,format&rect=0,76,2880,1646&w=1400&h=800",
-      },
-    },
-  },
-];
+const axios = require("axios");
 
-const result = data.map(({ image }) => {
-  const title = image.alt;
-  const xxl = { ...image.dimensions, url: image.url, type: "xxl" };
-  const xl = { ...image.xl.dimensions, url: image.xl.url, type: "xl" };
-  const lg = { ...image.lg.dimensions, url: image.lg.url, type: "lg" };
-  const md = { ...image.md.dimensions, url: image.md.url, type: "md" };
-  return { title, sources: [xxl, xl, lg, md] };
-});
+const JSON_DESTINATIONS =
+  "https://tripevolved.cdn.prismic.io/api/v2/documents/search?ref=ZLGtyhIAAHFXcZQg&q=%5B%5B%3Ad+%3D+at%28document.id%2C+%22ZE-x1xAAACkA4MxL%22%29+%5D%5D";
 
-console.log(JSON.stringify(result))
+const search = "vitoria";
+
+const parse = (data) =>
+  data.map(({ image }) => {
+    const title = image.alt;
+    const xxl = { ...image.dimensions, url: image.url, type: "xxl" };
+    const xl = { ...image.xl.dimensions, url: image.xl.url, type: "xl" };
+    const lg = { ...image.lg.dimensions, url: image.lg.url, type: "lg" };
+    const md = { ...image.md.dimensions, url: image.md.url, type: "md" };
+    return { title, sources: [xxl, xl, lg, md] };
+  });
+
+const main = async () => {
+  const { data } = await axios(JSON_DESTINATIONS);
+  const photos = data.results[0].data.photos;
+  const photo = photos.find(({ image }) => image.alt === search);
+  const result = parse([photo]);
+  console.log(JSON.stringify([result]));
+};
+
+main();
