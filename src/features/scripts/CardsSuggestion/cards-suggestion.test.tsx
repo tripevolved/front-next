9d1513a0893c50206125c8c67b4c5f74ca3ab13c
PGabriel-MB <pablogrbarbosa@gmail.com>
2023-07-28 08:07:25 -0300

    test adjust

--- src/features/scripts/CardsSuggestion/cards-suggestion.test.tsx
+++ src/features/scripts/CardsSuggestion/cards-suggestion.test.tsx
@@ -2,7 +2,8 @@ import type { CardsSuggestionProps } from "./cards-suggestion.types";
 import { render } from "@testing-library/react";
 import { CardSuggestion } from "./cards-suggestion.component";
 
-const makeSut = (props?: CardsSuggestionProps) => render(<CardSuggestion {...props} />);
+const makeSut = (props?: CardsSuggestionProps) =>
+  render(<CardSuggestion {...props} text="Teste" icon="attraction" onClick={() => null} />);
 
 describe("<CardsSuggestion>", () => {
   it("should render component", () => {
