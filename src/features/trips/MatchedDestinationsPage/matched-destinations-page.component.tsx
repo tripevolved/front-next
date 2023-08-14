9f59a44a402923b65a03c23c08e575a4dfe93dc1
PGabriel-MB <pablogrbarbosa@gmail.com>
2023-08-14 17:16:30 -0300

    finished visual correction

--- src/features/trips/MatchedDestinationsPage/matched-destinations-page.component.tsx
+++ src/features/trips/MatchedDestinationsPage/matched-destinations-page.component.tsx
@@ -5,11 +5,14 @@ import { useRouter } from "next/router";
 import { delay } from "@/utils/helpers/delay.helpers";
 
 import { EmptyState, GlobalLoader, SectionBase, Box, Text, StepsLoader } from "@/ui";
-import { Notification } from "mars-ds";
+import { Avatar, Notification } from "mars-ds";
 import { MatchedDestinationsPageProps } from "./matched-destinations-page.types";
 import { MatchedDestinationCard } from "./matched-destination-card.component";
 import { OtherChoicesCarousel } from "./other-choices-carousel.component";
 import { TripsApiService } from "@/services/api";
+import { PageAppBody } from "@/features/templates/PageAppBody";
+import { PageAppHeader } from "@/features";
+import { useAppStore } from "@/core/store";
 
 const EIGHT_SECONDS_IN_MS = 8 * 1000;
 const MILLISECONDS = EIGHT_SECONDS_IN_MS;
@@ -28,9 +31,16 @@ const STEPS = [
   },
 ];
 
-export const MatchedDestinationsPage = ({ className, sx, ...props }: MatchedDestinationsPageProps) => {
+export const MatchedDestinationsPage = ({
+  className,
+  sx,
+  ...props
+}: MatchedDestinationsPageProps) => {
   const { isLoading, data, error } = useMatchedDestinations();
   const cn = makeCn("has-trip", className)(sx);
+  const { name = "viajante" } = useAppStore((state) => state.travelerState);
+  const firstName = name.replace(/\s.*/, "");
+
   const [submitting, setSubmitting] = useState(false);
 
   const router = useRouter();
@@ -44,7 +54,10 @@ export const MatchedDestinationsPage = ({ className, sx, ...props }: MatchedDest
   const sendSetDestinationIdForTrip = async () => {
     try {
       setSubmitting(true);
-      const result = await TripsApiService.setDestinationId({ tripId: data?.tripId!, tripDestination: { destinationId: chosenDestination.current! }});
+      const result = await TripsApiService.setDestinationId({
+        tripId: data?.tripId!,
+        tripDestination: { destinationId: chosenDestination.current! },
+      });
     } catch (error) {
       setSubmitting(false);
       Notification.error("Devido à um erro não foi possível continuar");
@@ -60,42 +73,55 @@ export const MatchedDestinationsPage = ({ className, sx, ...props }: MatchedDest
       await router.replace("/app/viagens/criar/" + data?.tripId!);
     }
   };
-  
+
   if (error) return <EmptyState />;
   if (isLoading) return <GlobalLoader />;
 
   return (
-    <SectionBase >
-      {submitting ? (
+    <>
+      <PageAppHeader>
+        <div className="trip-details-panel__header">
+          <Avatar size="xl" thumbnail="/brand/logo-symbol-circle.svg" />
+          <div>
+            <Text heading as="div" size="sm" className="mb-xs">
+              Olá, <strong>{firstName}</strong> 👋
+            </Text>
+            <Text size="lg">Verique seus destinos</Text>
+          </div>
+        </div>
+      </PageAppHeader>
+      <PageAppBody>
+        {submitting ? (
           <StepsLoader steps={STEPS} milliseconds={MILLISECONDS} onFinish={handleFinish} />
         ) : (
-        <Box className={cn} {...props}>
-          <Text variant="heading" className="has-trip__header-title" size="sm">
-            Sua viagem ideal é para...
-          </Text>
-          <Box className="has-trip__trip-area">
-            <MatchedDestinationCard tripId={data?.tripId!} {...data?.mainChoice!} onChoice={handleCreateTrip} />
-          </Box>
-          {data?.otherChoices && data.otherChoices.length > 0 ? (
-            <>
-              <Box className="has-trip__recommendations-area">
+          <Box className={cn} {...props}>
+            <Text variant="heading" className="has-trip__header-title" size="sm">
+              Sua viagem ideal é para...
+            </Text>
+            <Box className="has-trip__trip-area">
+              <MatchedDestinationCard
+                tripId={data?.tripId!}
+                {...data?.mainChoice!}
+                onChoice={handleCreateTrip}
+              />
+            </Box>
+            {data?.otherChoices && data.otherChoices.length > 0 ? (
+              <Box className="has-trip__recommendations-area" style={{ paddingLeft: 0 }}>
                 <OtherChoicesCarousel
                   title="Outras opções"
                   recommendedDestinations={data.otherChoices.map((matchedDestination, i) => {
                     return {
                       tripId: data.tripId,
                       onChoice: handleCreateTrip,
-                      ...matchedDestination
+                      ...matchedDestination,
                     };
                   })}
                 />
               </Box>
-            </>
-          ) : (
-            <></>
-          )}
-        </Box>
-      )}
-    </SectionBase>
+            ) : null}
+          </Box>
+        )}
+      </PageAppBody>
+    </>
   );
-}
+};
