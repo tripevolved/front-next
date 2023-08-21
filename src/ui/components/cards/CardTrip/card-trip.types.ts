5cfeecb19af38ce52bb124f1776d8fddbf48d2bc
hmgasparotto <hmgasparotto@hotmail.com>
2023-08-21 19:54:04 -0300

    fix visuals for trip discover page

--- src/ui/components/cards/CardTrip/card-trip.types.ts
+++ src/ui/components/cards/CardTrip/card-trip.types.ts
@@ -1,9 +1,11 @@
 import type { ComponentHTMLProps } from "@/core/types";
 import type { PictureProps } from "@/ui";
+import { ReactNode } from "react";
 
 export interface CardTripProps extends ComponentHTMLProps {
-  status?: string;
+  header?: ReactNode;
   title?: string;
+  subtitle?: string;
   href?: string;
   image?: string | PictureProps;
 }
