73be254956a30106935507040be4216468912fd4
fredes <ricardofredes@gmail.com>
2023-08-21 20:44:39 -0300

    style: adjust back button in dark theme

--- src/features/templates/PageAppHeader/page-app-header.component.tsx
+++ src/features/templates/PageAppHeader/page-app-header.component.tsx
@@ -4,7 +4,15 @@ import type { PageAppHeaderProps } from "./page-app-header.types";
 
 import { makeCn } from "@/utils/helpers/css.helpers";
 
-export function PageAppHeader({ title, backButton, href, className, children, sx, ...props }: PageAppHeaderProps) {
+export function PageAppHeader({
+  title,
+  backButton,
+  href,
+  className,
+  children,
+  sx,
+  ...props
+}: PageAppHeaderProps) {
   const cn = makeCn("page-app-header", className)(sx);
 
   return (
@@ -15,20 +23,24 @@ export function PageAppHeader({ title, backButton, href, className, children, sx
             size="md"
             iconName="arrow-left"
             href={href}
-            className="mr-md"
+            className="mr-md theme-dark"
             variant="neutral"
           />
-        ) : <Avatar size="xl" thumbnail="/brand/logo-symbol-circle.svg" className="page-app-header__box__avatar" />}
+        ) : (
+          <Avatar
+            size="xl"
+            thumbnail="/brand/logo-symbol-circle.svg"
+            className="page-app-header__box__avatar"
+          />
+        )}
         {title ? (
           <Text heading size="xs" className="page-app-header__box__title">
             {title}
           </Text>
         ) : (
-          <>
-            {children}
-          </>
+          <>{children}</>
         )}
       </Box>
     </SectionBase>
   );
-};
+}
