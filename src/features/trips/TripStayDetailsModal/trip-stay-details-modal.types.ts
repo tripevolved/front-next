e61382c460ee7d3d3f4e9f7d6f7fe3dae0ee9f95
PGabriel-MB <pablogrbarbosa@gmail.com>
2023-08-26 15:25:53 -0300

    coding the room card

--- src/features/trips/TripStayDetailsModal/trip-stay-details-modal.types.ts
+++ src/features/trips/TripStayDetailsModal/trip-stay-details-modal.types.ts
@@ -3,5 +3,4 @@ import type { ComponentHTMLProps, TripStayDetails } from "@/core/types";
 export interface TripStayDetailsModalProps extends ComponentHTMLProps {
   stayData: TripStayDetails;
   name: string;
-  address: string;
 }
