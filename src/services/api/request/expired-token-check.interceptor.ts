0174a84175d377516c5b44bf343174ed06770289
fredes <ricardofredes@gmail.com>
2023-08-28 19:04:12 -0300

    feat(expired-token): add redirect to after login

--- src/services/api/request/expired-token-check.interceptor.ts
+++ src/services/api/request/expired-token-check.interceptor.ts
@@ -1,27 +1,21 @@
+import { useAppStore } from "@/core/store";
+import { initialUserState } from "@/core/store/user/user.constants";
 import { UserCredentials } from "@/services/user/credentials";
-import axios, { type AxiosResponse, AxiosError } from "axios";
+import type { AxiosResponse, AxiosError } from "axios";
 
-export const expiredTokenCheckInterceptor = async (config: AxiosResponse) => {
-  // TODO: Create some way to use redirectToSignIn here
-  try {
-    if (config.status === 401) {
-      UserCredentials.del();
-      window.location.replace("/app/entrar");
-    }
-  } catch (error) {
-    console.error(error);
-  }
+const handleExpiredToken = () => {
+  UserCredentials.del();
+  useAppStore.setState((state) => ({ ...state, user: initialUserState }));
+  const redirectTo = encodeURIComponent(`${location.pathname}${location.search}`);
+  location.replace(`/app/entrar?redirectTo=${redirectTo}`);
+};
+
+export const expiredTokenCheckInterceptor = async (config: AxiosResponse | AxiosError) => {
+  if (config.status === 401) handleExpiredToken();
   return config;
 };
 
 export const expiredTokenVerificationInterceptorError = (config: AxiosError) => {
-  try {
-    if (config.response?.status === 401) {
-      UserCredentials.del();
-      window.location.replace("/app/entrar");
-    }
-  } catch (error) {
-    console.error(error);
-  }
+  if (config.response?.status === 401) handleExpiredToken();
   return config;
 };
