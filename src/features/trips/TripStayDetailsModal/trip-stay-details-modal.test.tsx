838dbfcac987805b68cb2858e221ad10f7b17d84
PGabriel-MB <pablogrbarbosa@gmail.com>
2023-08-31 13:44:38 -0300

    test adjusted

--- src/features/trips/TripStayDetailsModal/trip-stay-details-modal.test.tsx
+++ src/features/trips/TripStayDetailsModal/trip-stay-details-modal.test.tsx
@@ -1,8 +1,55 @@
 import type { TripStayDetailsModalProps } from "./trip-stay-details-modal.types";
 import { render } from "@testing-library/react";
 import { TripStayDetailsModal } from "./trip-stay-details-modal.component";
+import { TripStayDetails } from "@/core/types";
 
-const makeSut = (props?: TripStayDetailsModalProps) => render(<TripStayDetailsModal {...props} />);
+const mockDetails: TripStayDetails = {
+  address: "Quadra QS 112",
+  checkInHour: "8h às 20h",
+  services: [
+    { title: "Ar condicionado", type: "ac" },
+    { title: "Boa cama", type: "bed" },
+    { title: "Café da Manhã", type: "breakfast" },
+    { title: "Wi-Fi", type: "wifi" },
+  ],
+  images: [
+    { url: "https://picsum.photos/300/200", altText: "Primeira imagem" },
+    { url: "https://picsum.photos/400/300", altText: "Segunda imagem" },
+    { url: "https://picsum.photos/500/400", altText: "Terceira imagem" },
+  ],
+  information: "Informação legal da acomodação",
+  price: 67.09,
+  currency: "R$",
+  rooms: [
+    {
+      coverImageUrl: "https://picsum.photos/300/200",
+      details: { amenities: ["coisa", "nova", "teste"], information: "informação sensacional" },
+      features: [
+        { title: "Wifi", type: "wifi" },
+        { title: "Ar Condicionado", type: "ac" },
+        { title: "Boa cama", type: "bed" },
+      ],
+      id: "i2u3g429",
+      isSelected: true,
+      price: 20.0,
+      subtitle: "Acomoda 2 pessoas",
+      title: "Suíte simples",
+    },
+    {
+      coverImageUrl: "https://picsum.photos/300/200",
+      details: { amenities: ["coisa", "nova", "teste"], information: "informação sensacional" },
+      features: [{ title: "Ar Condicionado", type: "ac" }],
+      id: "i2u3g429",
+      isSelected: true,
+      price: 20.0,
+      subtitle: "acomoda 4 pessoas",
+      title: "Suíte deluxe",
+    },
+  ],
+};
+
+const makeSut = (props?: TripStayDetailsModalProps) =>
+  render(<TripStayDetailsModal {...props} stayData={mockDetails} name="Teste" />);
 
 describe("<TripStayDetailsModal>", () => {
   it("should render component", () => {
--- src/features/trips/TripStayRoomDetailsModal/trip-stay-room-details-modal.test.tsx
+++ src/features/trips/TripStayRoomDetailsModal/trip-stay-room-details-modal.test.tsx
@@ -1,8 +1,25 @@
 import type { TripStayRoomDetailsModalProps } from "./trip-stay-room-details-modal.types";
 import { render } from "@testing-library/react";
 import { TripStayRoomDetailsModal } from "./trip-stay-room-details-modal.component";
+import { TripStayRoom } from "@/core/types";
 
-const makeSut = (props?: TripStayRoomDetailsModalProps) => render(<TripStayRoomDetailsModal {...props} />);
+const mockRoom: TripStayRoom = {
+  coverImageUrl: "https://picsum.photos/300/200",
+  details: { amenities: ["coisa", "nova", "teste"], information: "informação sensacional" },
+  features: [
+    { title: "Wifi", type: "wifi" },
+    { title: "Ar Condicionado", type: "ac" },
+    { title: "Boa cama", type: "bed" },
+  ],
+  id: "i2u3g429",
+  isSelected: true,
+  price: 20.0,
+  subtitle: "Acomoda 2 pessoas",
+  title: "Suíte simples",
+};
+
+const makeSut = (props?: TripStayRoomDetailsModalProps) =>
+  render(<TripStayRoomDetailsModal {...props} room={mockRoom} />);
 
 describe("<TripStayRoomDetailsModal>", () => {
   it("should render component", () => {
