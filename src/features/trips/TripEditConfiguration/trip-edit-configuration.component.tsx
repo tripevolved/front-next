d6a3992e5d0a4f98ae0c6ad16f3c25bcd2145e54
PGabriel-MB <pablogrbarbosa@gmail.com>
2023-09-06 20:42:54 -0300

    testing the request function

--- src/features/trips/TripEditConfiguration/trip-edit-configuration.component.tsx
+++ src/features/trips/TripEditConfiguration/trip-edit-configuration.component.tsx
@@ -1,15 +1,67 @@
+import { useRef, useState } from "react";
 import { StepConfiguration } from "../TripSteps/step-configuration";
 import { StepFinish } from "../TripSteps/step-finish";
 import type { TripEditConfigurationProps } from "./trip-edit-configuration.types";
 
 import { makeCn } from "@/utils/helpers/css.helpers";
+import { useAnimation } from "@/utils/hooks/animation.hook";
+import { Box, Text } from "@/ui";
 
-export function TripEditConfiguration({ className, children, sx, ...props }: TripEditConfigurationProps) {
+const CONFIG_STEPS = [
+  {
+    title: "Quando e quem?",
+    name: "configuration",
+    component: StepConfiguration,
+  },
+  {
+    title: "Finalização",
+    name: "finish",
+    component: StepFinish,
+  },
+];
+
+const DEFAULT_INITIAL_INDEX = 0;
+
+export function TripEditConfiguration({
+  className,
+  children,
+  sx,
+  ...props
+}: TripEditConfigurationProps) {
   const cn = makeCn("trip-edit-configuration", className)(sx);
+  const [currentIndex, setCurrentIndex] = useState(DEFAULT_INITIAL_INDEX);
+
+  const data = useRef<Record<string, any>>({});
+
+  const animation = useAnimation();
+
+  const handleNext = (newData?: Record<string, any>) => {
+    if (newData) {
+      data.current = { ...data.current, ...newData };
+    }
+    const nextIndex = currentIndex + 1;
+    if (nextIndex < CONFIG_STEPS.length) {
+      setCurrentIndex(nextIndex);
+      animation.trigger(true);
+    } else {
+      // Do something
+    }
+  };
+
+  const { component: Component } = CONFIG_STEPS[currentIndex];
 
   return (
-    <div className={cn} {...props}>
-      <StepConfiguration />
+    <div className={`${cn} gap-lg p-lg`} {...props}>
+      <Text heading style={{ color: "var(--color-brand-1)" }}>
+        Configurações da sua Viagem
+      </Text>
+      <Box style={animation.style}>
+        <Component
+          onNext={handleNext}
+          onPrevious={() => setCurrentIndex((state) => state - 1)}
+          goToStepName={() => undefined}
+        />
+      </Box>
     </div>
   );
-};
+}
---
+++ src/services/api/trip/configuration.ts
@@ -0,0 +1,49 @@
+import { ApiRequest } from "@/services/api/request";
+import { Travelers, TripDates, TripPeriod } from "./create";
+
+interface EditTripConfigurationRequest {
+  period: TripPeriod;
+  dates: TripDates;
+  maxBudget: number;
+  travelers: Travelers;
+}
+
+export type EditTripDto = {
+  days: number;
+  dates: [Date, Date];
+  maxBudget: number;
+  travelers: Pick<Travelers, "adults">;
+  tripId: string;
+};
+
+export type EditTrip = {
+  id: string;
+};
+
+export const editTrip = async ({
+  days,
+  dates,
+  maxBudget,
+  travelers,
+  tripId,
+}: EditTripDto): Promise<EditTrip> => {
+  const url = `trips/${tripId}/configuration`;
+
+  const parsedDates = {
+    startDate: dates[0],
+    endDate: dates[1],
+  } as TripDates;
+  const trip = {
+    period: { maxDays: days, minDays: days },
+    dates: parsedDates,
+    maxBudget,
+    travelers: {
+      adults: travelers.adults || 2,
+      children: 0,
+      type: 0,
+    },
+  } satisfies EditTripConfigurationRequest;
+  const { id } = await ApiRequest.post<EditTrip>(url, trip);
+
+  return { id };
+};
