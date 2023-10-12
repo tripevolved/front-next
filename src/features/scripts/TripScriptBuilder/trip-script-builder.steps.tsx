3864cce64eed4ee9194624988e49547bbaf28b77
hmgasparotto <hmgasparotto@hotmail.com>
2023-10-12 13:10:33 -0300

    add trip-type component

--- src/features/scripts/TripScriptBuilder/trip-script-builder.steps.tsx
+++ src/features/scripts/TripScriptBuilder/trip-script-builder.steps.tsx
@@ -1,10 +1,16 @@
 import type { TemplateStepsBuilderProps } from "@/features";
 import { InitialStep } from "./trip-script-builder-initial.component";
+import { TripTypeStep } from "./trip-type-step.component";
 
 export const SCRIPT_BUILDER_STEPS: TemplateStepsBuilderProps["steps"] = [
   {
     title: "Contruir meu roteiro",
     name: "initial",
     component: InitialStep,
+  },
+  {
+    title: "Que tipo de viagem você quer?",
+    name: "trip-type",
+    component: TripTypeStep,
   }
 ];
