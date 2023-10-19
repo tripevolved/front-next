da70baaa04d12f87c570d881bee4fe6fedcf36b7
hmgasparotto <hmgasparotto@hotmail.com>
2023-10-19 17:31:50 -0300

    fix build

--- src/core/store/trip-day/trip-day.constants.ts
+++ src/core/store/trip-day/trip-day.constants.ts
@@ -1,7 +1,8 @@
-import type { TripScriptDay, TripScriptAction } from "@/core/types";
+import type { TripScriptDay } from "@/core/types";
 
 export const initialTripScriptDayValue: TripScriptDay = {
   id: "",
+  day: 0,
   date: "",
   actions: [],
   details: {
