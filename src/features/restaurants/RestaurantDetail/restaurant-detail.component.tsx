f43005286db954f196f6a0b5e2788dff72943b2d
hmgasparotto <hmgasparotto@hotmail.com>
2023-10-19 17:16:33 -0300

    adjust stiles and include carousel

--- src/features/restaurants/RestaurantDetail/restaurant-detail.component.tsx
+++ src/features/restaurants/RestaurantDetail/restaurant-detail.component.tsx
@@ -1,5 +1,5 @@
 import { Button, Divider, Grid, Loader } from "mars-ds";
-import { Box, ErrorState, Text } from "@/ui";
+import { Box, Carousel, Emoji, ErrorState, Picture, Text } from "@/ui";
 import { RestaurantsApiService } from "@/services/api";
 import useSwr from "swr";
 import { RestaurantDetail } from "@/core/types";
@@ -33,18 +33,45 @@ export const RestaurantDetailComponent = ({ restaurantId, onInclude, onDiscard }
 
   return (
     <Grid gap={16} className="restaurant-detail">
-      <Text size="sm" heading as="p" className="restaurant-detail__title">{data.name}</Text>
+      <Text size="sm" heading as="h1" className="restaurant-detail__title">{data.name}</Text>
       <Text size="md" as="p" className="restaurant-detail__subtitle">{`${data.tags}${data.priceRange && ` - ${data.priceRange}`}`}</Text>
-      {/* TODO: insert image carousel */}
+      {data.images?.length ? (
+        <Carousel>
+          {data.images.map((image, key) => (
+            <Picture
+              className="restaurant-detail__image"
+              src={image.url}
+              alt={image.alt ?? ""}
+              key={key}
+            />
+          ))}
+        </Carousel>
+      ) : (
+        <Picture
+          style={{
+            backgroundColor: "var(--color-brand-1)",
+            display: "flex",
+            justifyContent: "center",
+          }}
+          className="restaurant-detail__image"
+          src={"/assets/blank-image.png"}
+        />
+      )}
       <Box className="restaurant-detail__info">
-        <Text size="sm" heading as="p" className="restaurant-detail__info__title">Informações</Text>
-        <Text size="md" as="p" className="restaurant-detail__info__description">{data.description}</Text>
-        <Divider />
-        <Text size="sm" heading as="p" className="restaurant-detail__hours">{data.functioningHoursDetail}</Text>
-        <Text size="md" as="p" className="restaurant-detail__address">{data.address}</Text>
+        <Text size="xl" as="h2" className="restaurant-detail__info__title">Informações</Text>
+        <Text size="sm" as="p" className="restaurant-detail__info__description">{data.description}</Text>
+        <Divider className="restaurant-detail__info__divider" />
+        <Grid columns={[1,24]} className="restaurant-detail__info__subinfo">
+          <Picture src="/assets/stays/time.png" />
+          <Text size="md" as="p">{data.functioningHoursDetail}</Text>
+        </Grid>
+        <Grid columns={[1,24]} className="restaurant-detail__info__subinfo">
+          <Picture src="/assets/stays/pin.png" />
+          <Text size="md" as="p">{data.address}</Text>
+        </Grid>
       </Box>
-      <Button iconName="thumbs-up" onClick={onInclude} variant="primary">Incluir no roteiro</Button>
-      <Button iconName="thumbs-down" onClick={onDiscard} variant="naked" size="sm">Descartar</Button>
+      <Button iconName="thumbs-up" onClick={onInclude} variant="primary" className="restaurant-detail__button">Incluir no roteiro</Button>
+      <Button iconName="thumbs-down" onClick={onDiscard} variant="naked" size="sm" className="restaurant-detail__button">Descartar</Button>
     </Grid>
   );
 };
