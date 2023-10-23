b440b7adf29acd49dcf281573e093b344ceee03b
hmgasparotto <hmgasparotto@hotmail.com>
2023-10-23 17:24:34 -0300

    improve trip loading

--- src/features/trips/TripDetailsPage/trip-details.hook.tsx
+++ src/features/trips/TripDetailsPage/trip-details.hook.tsx
@@ -5,23 +5,27 @@ import useSWR from "swr";
 
 const REFRESH_INTERVAL = 3000; // 3 seconds
 const NOT_REFRESH = 0;
+const MAX_REFRESH_COUNT = 5;
 
 export const useTripDetails = () => {
   const [isBuilding, setIsBuilding] = useState(false);
+  const [refreshCount, setRefreshCount] = useState(0);
   const router = useRouter();
   const tripId = typeof router.query.id === "string" ? router.query.id : "";
 
   const { isLoading, data, error } = useSWR(
     `trip/${tripId}`,
-    () => TripsApiService.getById(tripId),
-    { refreshInterval: isBuilding ? REFRESH_INTERVAL : NOT_REFRESH }
+    () => { setRefreshCount(refreshCount + 1); return TripsApiService.getById(tripId); },
+    { refreshInterval: isBuilding && refreshCount <= MAX_REFRESH_COUNT ? REFRESH_INTERVAL : NOT_REFRESH }
   );
 
   useEffect(() => {
-    if (!isLoading) setIsBuilding(!!data?.isBuilding);
+    if (!isLoading) { 
+      setIsBuilding(!!data?.isBuilding);
+    }
   }, [data, isLoading]);
 
   const isEmpty = !isLoading && !isBuilding && (!data || error);
 
-  return { isLoading, data, error, isBuilding, isEmpty };
+  return { isLoading, data, error, isBuilding, isEmpty, refreshCount };
 };
