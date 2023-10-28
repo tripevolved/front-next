fb4793c24cceb17f0b1d30fe16ef4957ee6cdc2c
hmgasparotto <hmgasparotto@hotmail.com>
2023-10-28 10:20:24 -0300

    fix forced error

--- src/services/api/trip/price.ts
+++ src/services/api/trip/price.ts
@@ -2,7 +2,7 @@ import { TripPrice } from "@/core/types";
 import { ApiRequest } from "@/services/api/request";
 
 export const getTripPriceById = async (tripId: string): Promise<TripPrice> => {
-  const route = `trips/${tripId}/price1`;
+  const route = `trips/${tripId}/price`;
   const tripPrice = await ApiRequest.get<Omit<TripPrice, "total">>(route);
   const total = tripPrice.price + tripPrice.serviceFee;
   return { ...tripPrice, total };
