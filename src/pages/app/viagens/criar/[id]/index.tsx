fc71e6b3758a6db8348a49dbf097c487bc78452a
hmgasparotto <hmgasparotto@hotmail.com>
2023-11-01 11:22:33 -0300

    fix visual for tripdetails page

--- src/pages/app/viagens/criar/[id]/index.tsx
+++ src/pages/app/viagens/criar/[id]/index.tsx
@@ -1,13 +1,7 @@
 import { TripDetailsPage } from "@/features";
-import { PageApp } from "@/features";
 
 export default function TripDetailsPageRoute() {
   return (
-    <PageApp
-      headerOptions={{ title: "Viagem", backUrl: "/app/painel" }}
-      seo={{ title: "Detalhes de compra da viagem" }}
-    >
-      <TripDetailsPage />
-    </PageApp>
+    <TripDetailsPage />
   );
 }
