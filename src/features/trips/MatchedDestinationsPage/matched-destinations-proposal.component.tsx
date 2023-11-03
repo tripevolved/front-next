7a2dba37fdb696e26337df56bc963b0ee243f898
fredes <ricardofredes@gmail.com>
2023-11-02 23:52:09 -0300

    feat: improve modal

--- src/features/trips/MatchedDestinationsPage/matched-destinations-proposal.component.tsx
+++ src/features/trips/MatchedDestinationsPage/matched-destinations-proposal.component.tsx
@@ -1,7 +1,7 @@
 import type { MatchedDestination } from "@/services/api/trip/matches";
 
 import { Text } from "@/ui";
-import { Grid } from "mars-ds";
+import { AutoScroll, Grid } from "mars-ds";
 import { MatchedDestinationCard } from "./matched-destination-card.component";
 
 export interface MatchedDestinationsProposalProps {
@@ -43,16 +43,17 @@ export const MatchedDestinationsProposal = ({
           <Text className="mt-lg" as="h2" heading size="xs">
             Outras opções
           </Text>
-          <Grid columns={{ xs: 2, md: 3, lg: 4 }}>
+          <div className="matched-destinations-cards">
             {otherChoices.map((choice, key) => (
-              <MatchedDestinationCard
-                key={key}
-                {...choice}
-                travelersNumber={choice.travelers ?? 2}
-                onChoice={() => handleCreateTrip(choice.destinationId)}
-              />
+              <div key={key}>
+                <MatchedDestinationCard
+                  {...choice}
+                  travelersNumber={choice.travelers ?? 2}
+                  onChoice={() => handleCreateTrip(choice.destinationId)}
+                />
+              </div>
             ))}
-          </Grid>
+          </div>
         </>
       ) : null}
     </Grid>
