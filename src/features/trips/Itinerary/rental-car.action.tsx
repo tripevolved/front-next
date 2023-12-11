33b4f139843028c3deed91455d4ef7462c739ad2
hmgasparotto <hmgasparotto@hotmail.com>
2023-12-11 09:40:52 -0300

    fix and improve rental-car view

--- src/features/trips/Itinerary/rental-car.action.tsx
+++ src/features/trips/Itinerary/rental-car.action.tsx
@@ -1,33 +1,19 @@
 import type { ItineraryAction as ItineraryActionProps } from "@/core/types/itinerary";
 
-import { Accordion, Skeleton } from "mars-ds";
-import { Text, CardHighlight } from "@/ui";
-import { TripDetailInfo } from "../TripDetailsPage";
+import { CardHighlight } from "@/ui";
 import { getWhatsappLink } from "@/utils/helpers/whatsapp.helpers";
 
 export const RentalCarAction = (props: ItineraryActionProps) => {
   return (
-    <Accordion title={`🌑 ${props?.from.title || "Aluguel de carro"}`}>
-      <Skeleton height={170}>
-        <div className="w-100 pl-lg itinerary__item" style={{ marginLeft: 6 }}>
-          <TripDetailInfo image={`/assets/destino/carro.svg`} title="Aluguel de Carro">
-            <Text style={{ color: "var(--color-gray-1)" }}>
-              Sua rota iniciará em {props.from.title} até serguirá até {props.to.title}
-            </Text>
-            <CardHighlight
-              variant="default"
-              heading="Esta parte do trajeto será feita por terra"
-              text="Gostaria de alugar um veículo com nossa equipe?"
-              cta={{
-                href: getWhatsappLink("Preciso alugar um carro para minha trip"),
-                label: "Preciso alugar um carro",
-                iconName: "whatsapp",
-                isRtl: true,
-              }}
-            />
-          </TripDetailInfo>
-        </div>
-      </Skeleton>
-    </Accordion>
+    <CardHighlight
+      variant="warning"
+      text="Gostaria de alugar um veículo? Estamos à disposição para te ajudar!"
+      cta={{
+        href: getWhatsappLink("Preciso alugar um carro para minha trip"),
+        label: "Preciso alugar um carro",
+        iconName: "whatsapp",
+        isRtl: true,
+      }}
+    />
   );
 };
