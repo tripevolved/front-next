ffdfa6d09ab1cc07c5161c79a0f917a9826bf7dc
hmgasparotto <hmgasparotto@hotmail.com>
2023-12-26 14:05:11 -0300

    fix price property

--- src/services/api/trip/price.ts
+++ src/services/api/trip/price.ts
@@ -1,10 +1,8 @@
 import { TripPrice } from "@/core/types";
 import { ApiRequest } from "@/services/api/request";
-import { MathHelper } from "@/utils/helpers/math.helper";
 
 export const getTripPriceById = async (tripId: string): Promise<TripPrice> => {
   const route = `trips/${tripId}/price`;
-  const tripPrice = await ApiRequest.get<Omit<TripPrice, "total">>(route);
-  const total = MathHelper.sum(tripPrice.price, tripPrice.serviceFee);
-  return { ...tripPrice, total };
+  const tripPrice = await ApiRequest.get<TripPrice>(route);
+  return tripPrice;
 };
