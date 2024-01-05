3cc374b2642b7c4c60dd67f841e5ec4c7f04edb2
hmgasparotto <hmgasparotto@hotmail.com>
2024-01-05 14:42:06 -0300

    new structure for attractions modal

--- src/features/scripts/SeeAttractionDetailsModal/see-attraction-details-modal.component.tsx
+++ src/features/scripts/SeeAttractionDetailsModal/see-attraction-details-modal.component.tsx
@@ -1,12 +1,12 @@
-import { Text, Box, ErrorState, GlobalLoader, EmptyState } from "@/ui";
+import { Text, Box, ErrorState, GlobalLoader, EmptyState, Picture, Carousel } from "@/ui";
 import type { SeeAttractionDetailsModalProps } from "./see-attraction-details-modal.types";
 
 import { makeCn } from "@/utils/helpers/css.helpers";
-import { Button, Divider, Icon } from "mars-ds";
-import { formatByDataType } from "@/utils/helpers/number.helpers";
+import { Button, Divider, Grid, Icon, Label, LabelVariants, RatingStar } from "mars-ds";
 import { TripScriptsApiService } from "@/services/api";
 import { AttractionDetail } from "@/core/types";
 import useSWR from "swr";
+import { parsePhoto } from "@/utils/helpers/photo.helpers";
 
 export function SeeAttractionDetailsModal({
   className,
@@ -49,26 +49,53 @@ export function SeeAttractionDetailsModal({
         <Text heading className="see-attraction-details-modal__header__title">
           {attraction.name}
         </Text>
+        <Grid columns={{sm: 2, md: 4}}>
+          {data.ratings && data.ratings.map((rating, index) => (
+            <Label key={index} variant={(rating.rating >= 3.8 ? LabelVariants.Success : (rating.rating <= 1.0 ? LabelVariants.Error : LabelVariants.Warning))}>
+              {rating.feature}
+            </Label>
+          ))}
+        </Grid>
         <Text className="see-attraction-details-modal__header__subtitle">
           {data.tags}
         </Text>
       </Box>
-      {data.description && (
-        <Box className="see-attraction-details-modal__body">
-          <Text size="xxl" className="see-attraction-details-modal__body__title">
-            Informações
-          </Text>
-          <Text className="see-attraction-details-modal__body__description">
-            {data.description}
-          </Text>
-        </Box>
-      )}
+      <Box className="see-attraction-details-modal__body">
+        {data.images?.length && data.images.length > 0 ? (
+            <Carousel height={300}>
+              {data.images.map((image, key) => (
+                <Picture
+                  key={key}
+                >
+                  {parsePhoto(image)}
+                </Picture>
+              ))}
+            </Carousel>
+          ) : (<></>)}
+        {data.images?.length && data.images.length > 0 && data.description ? <Divider style={{margin: "16px 0"}} /> : <></>}
+        {data.description && (
+          <>
+            <Text size="xxl" className="see-attraction-details-modal__body__title">
+              Informações
+            </Text>
+            <Text className="see-attraction-details-modal__body__description">
+              {data.description}
+            </Text>
+          </>
+        )}
+      </Box>
       <Divider />
       <Box className="see-attraction-details-modal__info-list">
         {data.availabilityInfo && (
           <div className="see-attraction-details-modal__info-list__item">
             <Icon size="sm" color="var(--color-brand-3)" name="clock" />
             <Text size="md">{data.availabilityInfo}</Text>
+          </div>
+        )}
+        {data.restrictions && (
+          <div className="see-attraction-details-modal__info-list__item">
+            <Icon size="sm" color="var(--color-brand-3)" name="lock" />
+            <Text size="md">{data.restrictions}</Text>
           </div>
         )}
         {attraction.address && (
