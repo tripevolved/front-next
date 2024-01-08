e5c2647e7719fbc679e636a74ff06ac383e92ed4
hmgasparotto <hmgasparotto@hotmail.com>
2024-01-08 11:10:21 -0300

    implement user sign up with uniqueId

---
+++ src/features/auth/AuthRegisterForm/auth-register.form.tsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+
+import { TextField } from "mars-ds";
+import { AuthFormSection } from "../AuthFormSection";
+import { SubmitHandler } from "@/utils/helpers/form.helpers";
+import { UserApiService } from "@/services/api/user";
+import { useAppStore } from "@/core/store";
+import { LeadCreateDTO } from "@/core/types";
+
+
+export function AuthRegisterForm() {
+  const [submitting, setSubmitting] = useState(false);
+  const { leadCreate, lead } = useAppStore();
+  const router = useRouter();
+
+  const handleSubmit: SubmitHandler<LeadCreateDTO> = async (data) => {
+    setSubmitting(true);
+    try {
+      const newLead = await UserApiService.uniqueSignUp(data);
+      leadCreate(newLead);
+      router.replace(`/app/cadastro/${newLead.uniqueId}?email=${encodeURIComponent(newLead.email)}`);
+    } catch (error) {
+      console.error(error);
+      setSubmitting(false);
+    }
+  };
+
+  return (
+    <AuthFormSection
+      heading="Insira seus dados de cadastro para continuar"
+      submitting={submitting}
+      onSubmitHandler={handleSubmit}
+      submitButton={{ children: "Continuar" }}
+    >
+      <TextField required name="name" label="Nome" minLength={3} />
+      <TextField required name="email" label="Seu melhor e-mail" type="email" />
+      <TextField
+        required
+        name="phone"
+        label="Seu WhatsApp"
+        type="tel"
+        minLength={14}
+        mask="(99) 99999-9999"
+      />
+      <input type="hidden" name="inviterId" value={lead.invitedBy?.id} />
+      <input type="hidden" name="inviterEmail" value={lead.invitedBy?.email} />
+      <input type="hidden" name="affiliateId" value={lead.invitedBy?.affiliateId} />
+    </AuthFormSection>
+  );
+}
--- src/services/api/user/index.ts
+++ src/services/api/user/index.ts
@@ -3,5 +3,6 @@ export type { LoginDTO } from "./login";
 import { login } from "./login";
 import { forgotPassword } from "./password";
 import { signUp } from "./sign-up";
+import { uniqueSignUp } from "./unique-sign-up";
 
-export const UserApiService = { login, forgotPassword, signUp };
+export const UserApiService = { login, forgotPassword, signUp, uniqueSignUp };
