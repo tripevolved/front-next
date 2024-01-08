21c9aaba50b52814a6cf0c5bc17a913e6802650b
hmgasparotto <hmgasparotto@hotmail.com>
2024-01-08 12:19:39 -0300

    add structure to generate new uniqueid

--- src/services/api/user/index.ts
+++ src/services/api/user/index.ts
@@ -4,5 +4,6 @@ import { login } from "./login";
 import { forgotPassword } from "./password";
 import { signUp } from "./sign-up";
 import { uniqueSignUp } from "./unique-sign-up";
+import { resetUniqueSignUp } from "./reset-unique-sign-up";
 
-export const UserApiService = { login, forgotPassword, signUp, uniqueSignUp };
+export const UserApiService = { login, forgotPassword, signUp, uniqueSignUp, resetUniqueSignUp };
