168a5260e61f16ad48c22d0efc2e5c150be32b2b
hmgasparotto <hmgasparotto@hotmail.com>
2024-01-08 12:41:35 -0300

    finish implementatino for reset password

--- src/features/auth/AuthForgotPassword/auth-forgot-password.component.tsx
+++ src/features/auth/AuthForgotPassword/auth-forgot-password.component.tsx
@@ -1,24 +1,39 @@
 import { useState } from "react";
-import { Link, Notification, TextField } from "mars-ds";
+import { Link, Notification, PasswordField, PasswordStrongField, TextField } from "mars-ds";
 import { Text } from "@/ui";
-
-import { AuthSection } from "../AuthSection";
 import { AuthFormSection } from "../AuthFormSection";
 
 import { SubmitHandler } from "@/utils/helpers/form.helpers";
 import { UserApiService } from "@/services/api/user";
+import { useRouter } from "next/router";
+
+type PasswordResetData = Record<string, string>;
 
 export function AuthForgotPassword() {
   const [submitting, setSubmitting] = useState(false);
   const [success, setSuccess] = useState(false);
+  const [passwordOK, setPasswordOK] = useState(false);
+  const [userId, setUserId] = useState<string>();
+  const [data, setData] = useState<PasswordResetData>({});
+
+  const router = useRouter();
+
+  const passwordConfirmationOK = data["password"] === data["passwordConfirmation"];
+  const isValid = passwordOK && passwordConfirmationOK;
+
+  const handleChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const { value, name } = event.target as HTMLInputElement;
+    setData((state: PasswordResetData) => ({ ...state, [name]: value }));
+  };
 
   const handleSubmit: SubmitHandler<{ email: string }> = async ({ email }) => {
     try {
       setSubmitting(true);
-      await UserApiService.forgotPassword(email);
+      const { userId } = await UserApiService.forgotPassword(email);
+      setUserId(userId);
+      setSubmitting(false);
       setSuccess(true);
     } catch (error) {
-      console.error(error);
       setSubmitting(false);
       Notification.error(
         "Não foi possível continuar. Verifique se o e-mail informado está correto."
@@ -26,11 +41,40 @@ export function AuthForgotPassword() {
     }
   };
 
+  const handleSubmitReset: SubmitHandler<{ password: string, confirmationCode: string }> = async ({ password, confirmationCode}) => {
+    setSubmitting(true);
+    await UserApiService.resetPassword(userId ?? "", password, confirmationCode);
+    Notification.success("Senha alterada com sucesso!");
+    router.replace("/app/entrar");
+  };
+
   if (success) {
     return (
-      <AuthSection withCard heading="E-mail envidado!">
-        <Text>Em breve um e-mail com o novo link chegará eu sua caixa de entrada.</Text>
-      </AuthSection>
+      <AuthFormSection
+        heading="E-mail enviado!"
+        submitting={submitting}
+        submitButton={{ label: "Resetar senha", disabled: !isValid }}
+        onSubmitHandler={handleSubmitReset}
+      >
+        <Text size="lg">Em breve um e-mail com o novo link chegará eu sua caixa de entrada.</Text>
+        <Text size="sm">Então, basta você informar o código de verificação recebido.</Text>
+        <TextField name="confirmationCode" type="number" label="Código de verificação" required />
+        <PasswordStrongField
+          name="password"
+          required
+          onValid={setPasswordOK}
+          onChange={handleChange}
+        />
+        <PasswordField
+          label="Confirme a sua senha"
+          name="passwordConfirmation"
+          minLength={8}
+          onChange={handleChange}
+          error={!passwordConfirmationOK}
+          required
+          disabled={submitting}
+        />
+      </AuthFormSection>
     );
   }
 
@@ -46,7 +90,7 @@ export function AuthForgotPassword() {
       </Link>
       <Text>
         Para redefinir sua senha, informe o seu endereço de e-mail cadastrado para a sua conta e lhe
-        enviaremos por e-mail um link com as instruções.
+        enviaremos por e-mail um código de verificação.
       </Text>
       <TextField name="email" type="email" label="E-mail" required />
     </AuthFormSection>
--- src/services/api/user/index.ts
+++ src/services/api/user/index.ts
@@ -1,9 +1,9 @@
 export type { LoginDTO } from "./login";
 
 import { login } from "./login";
-import { forgotPassword } from "./password";
+import { forgotPassword, resetPassword } from "./password";
 import { signUp } from "./sign-up";
 import { uniqueSignUp } from "./unique-sign-up";
 import { resetUniqueSignUp } from "./reset-unique-sign-up";
 
-export const UserApiService = { login, forgotPassword, signUp, uniqueSignUp, resetUniqueSignUp };
+export const UserApiService = { login, forgotPassword, resetPassword, signUp, uniqueSignUp, resetUniqueSignUp };
