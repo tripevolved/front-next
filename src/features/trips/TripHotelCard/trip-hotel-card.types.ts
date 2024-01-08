553155e6e72eb40b3a35f10437653b7bb7453bdd
PGabriel-MB <pablogrbarbosa@gmail.com>
2024-01-08 16:38:16 -0300

    build correction

--- src/features/trips/TripHotelCard/trip-hotel-card.types.ts
+++ src/features/trips/TripHotelCard/trip-hotel-card.types.ts
@@ -2,7 +2,7 @@ import type { TripStay } from "@/core/types";
 import { NextRouter } from "next/router";
 
 export interface TripHotelCardProps {
-  tripStayData: Omit<TripStay, "highlight"> & { coverImageUrl: string };
+  tripStayData: Omit<TripStay, "highlight"> & { coverImageUrl?: string };
   tripId: string;
   isCurated?: boolean;
   onSelect: () => void;
