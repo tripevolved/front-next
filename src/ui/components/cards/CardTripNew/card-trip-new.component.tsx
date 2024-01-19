17d598ef894fdc842db7ff6416ec8f24fdfa9615
hmgasparotto <hmgasparotto@hotmail.com>
2024-01-19 15:24:15 -0300

    create component for tooltip on hover

--- src/ui/components/cards/CardTripNew/card-trip-new.component.tsx
+++ src/ui/components/cards/CardTripNew/card-trip-new.component.tsx
@@ -1,18 +1,29 @@
 import { Button, Link } from "mars-ds";
-import { IconCustom, Text } from "@/ui";
+import { HoverTooltipCard, IconCustom } from "@/ui";
 import type { CardTripNewProps } from "./card-trip-new.types";
+import { useAppStore } from "@/core/store";
 
 export const CardTripNew = ({ href, iconName, title }: CardTripNewProps) => {
-  return (
-    <Link href={href} className="card-trip card-trip-new">
-      <div className="card-trip-new__content">
-        <div className="card-trip-new__image">
-          <IconCustom name={iconName} />
-        </div>
-        <Button href={href} variant="neutral" iconName="plus" size="sm">
-          {title}
-        </Button>
+  const { availableFeatures } = useAppStore((state) => state.travelerState);
+  const enableNewTrip = false; //availableFeatures.includes("ITINERARY");
+
+  const childrenComponent = (
+    <div className="card-trip-new__content">
+      <div className="card-trip-new__image">
+        <IconCustom name={iconName} />
       </div>
+      <Button href={href} variant="neutral" iconName={enableNewTrip ? "plus" : "lock"} size="sm" disabled={!enableNewTrip}>
+        {title}
+      </Button>      
+    </div>);
+
+  return enableNewTrip ? (
+    <Link href={href} className="card-trip card-trip-new">
+      {childrenComponent}
     </Link>
+  ) : (
+    <div className="card-trip card-trip-new">
+      <HoverTooltipCard text="Essa funcionalidade ainda não está habilitada para você. Fale conosco e vamos ajudar a construir sua trip!">{childrenComponent}</HoverTooltipCard>
+    </div>
   );
 };
---
+++ src/ui/components/cards/HoverTooltipCard/index.ts
@@ -0,0 +1,6 @@
+/**
+ * Automatically generated
+ * run "yarn component:index" to update
+ */
+export * from "./hover-tooltip-card.component";
+export * from "./hover-tooltip-card.types";
