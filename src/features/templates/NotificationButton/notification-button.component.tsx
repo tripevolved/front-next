7aaea25b22c58ff1a4e7bfd026bc7e62ffc22a75
PGabriel-MB <pablogrbarbosa@gmail.com>
2024-02-27 08:14:36 -0300

    adds the notification bullet

--- src/features/templates/NotificationButton/notification-button.component.tsx
+++ src/features/templates/NotificationButton/notification-button.component.tsx
@@ -1,4 +1,5 @@
 import { ToggleButton } from "mars-ds";
+import useSWR from "swr";
 import type { NotificationButtonProps } from "./notification-button.types";
 
 import { makeCn } from "@/utils/helpers/css.helpers";
@@ -9,13 +10,17 @@ export function NotificationButton({ className, children, sx, href, ...props }: 
   const fetcher = async () => {}
 
   return (
-    <ToggleButton
-      {...props}
-      href={href}
-      variant="text"
-      iconName="bell"
-      title="Notificações"
-      onClick={() => null}
-    />
+    <div className={cn}>
+      <ToggleButton
+        {...props}
+        href={href}
+        variant="text"
+        iconName="bell"
+        title="Notificações"
+        onClick={() => null}
+        className="notification-button__button"
+      />
+      <span className="notification-button__bullet">⏺</span>
+    </div>
   );
 };
