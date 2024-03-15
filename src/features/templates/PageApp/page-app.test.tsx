3082891d04ba2b0988f74a9f8a509a29276bc951
PGabriel-MB <pablogrbarbosa@gmail.com>
2024-03-15 15:01:56 -0300

    tests adjusts

--- src/features/templates/PageApp/page-app.test.tsx
+++ src/features/templates/PageApp/page-app.test.tsx
@@ -1,7 +1,9 @@
 import type { PageAppProps } from "./page-app.types";
 import { render } from "@testing-library/react";
 import { PageApp } from "./page-app.component";
+import { mockUseRouter } from "@/utils/mocks/next-router.mock";
 
+mockUseRouter();
 const makeSut = (props?: PageAppProps) => render(<PageApp {...props}>FAKE_CHILDREN</PageApp>);
 
 describe("<PageApp>", () => {
