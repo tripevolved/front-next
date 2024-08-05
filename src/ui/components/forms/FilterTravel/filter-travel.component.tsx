d28d0fa71f72b9afff3a8008a23580e40ccfbba2
Vinicius de Oliveira <vinicius.oliveira@itschimitz.com>
2024-08-05 14:02:07 -0300

    feat: diagramacao lista destinos com filtro

--- src/ui/components/forms/FilterTravel/filter-travel.component.tsx
+++ src/ui/components/forms/FilterTravel/filter-travel.component.tsx
@@ -1,66 +1,49 @@
 import React, { useState, useEffect } from "react";
 import { Button, Icon } from "mars-ds";
 import { FilterTravelProps } from "./filter-travel.types";
-import { getMatches } from "@/services/api/trip/matches";
 
-export const FilterTravel = ({ options, buttonText }: FilterTravelProps) => {
+export const FilterTravel = ({ options, buttonText, onFetchResults }: FilterTravelProps) => {
   const [formData, setFormData] = useState({
-    travelerType: "",
-    travelPurpose: "",
-    duration: "",
+    travelerProfile: "",
+    objectiveId: "",
+    days: "",
     budget: ""
   });
-
-  const fetchMatchedDestinations = async () => {
-    const response = await getMatches({
-      travelerType: formData.travelerType,
-      travelPurpose: formData.travelPurpose,
-      duration: formData.duration,
-      budget: formData.budget,
-    });
-  };
-
-  useEffect(() => {
-    if (options) {
-      setFormData({
-        travelerType: options.travelerTypes[0].value,
-        travelPurpose: options.travelPurposes[0].value,
-        duration: options.durations[0].value,
-        budget: options.budgets[0].value
-      });
-    }
-  }, []);
 
   const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
 
+  const handleFetchClick = () => {
+    onFetchResults(formData);
+  };
+
   return (
     <div className="box-filter-travel">
       <div className="select-wrapper">
         <Icon name="user" className="custom-icon-filter-travel" />
-        <select name="travelerType" value={formData.travelerType} onChange={handleChange}>
-          {options.travelerTypes.map((type) => (
-            <option key={type.value} value={type.value}>{type.label}</option>
+        <select name="travelerProfile" value={formData.travelerProfile} onChange={handleChange}>
+          {options?.travelerProfiles.map((profile) => (
+            <option key={profile.value} value={profile.value}>{profile.label}</option>
           ))}
         </select>
         <Icon name="chevron-down" className="custom-icon-chevron-down" />
       </div>
       <div className="select-wrapper">
         <Icon name="target" className="custom-icon-filter-travel" />
-        <select name="travelPurpose" value={formData.travelPurpose} onChange={handleChange}>
-          {options.travelPurposes.map((purpose) => (
-            <option key={purpose.value} value={purpose.value}>{purpose.label}</option>
+        <select name="objectiveId" value={formData.objectiveId} onChange={handleChange}>
+          {options?.objectiveIds.map((objective) => (
+            <option key={objective.value} value={objective.value}>{objective.label}</option>
           ))}
         </select>
         <Icon name="chevron-down" className="custom-icon-chevron-down" />
       </div>
       <div className="select-wrapper">
         <Icon name="clock" className="custom-icon-filter-travel" />
-        <select name="duration" value={formData.duration} onChange={handleChange}>
-          {options.durations.map((duration) => (
-            <option key={duration.value} value={duration.value}>{duration.label}</option>
+        <select name="days" value={formData.days} onChange={handleChange}>
+          {options?.days.map((day) => (
+            <option key={day.value} value={day.value}>{day.label}</option>
           ))}
         </select>
         <Icon name="chevron-down" className="custom-icon-chevron-down" />
@@ -68,13 +51,13 @@ export const FilterTravel = ({ options, buttonText }: FilterTravelProps) => {
       <div className="select-wrapper">
         <Icon name="dollar-sign" className="custom-icon-filter-travel" />
         <select name="budget" value={formData.budget} onChange={handleChange}>
-          {options.budgets.map((budget) => (
+          {options?.budgets.map((budget) => (
             <option key={budget.value} value={budget.value}>{budget.label}</option>
           ))}
         </select>
         <Icon name="chevron-down" className="custom-icon-chevron-down" />
       </div>
-      <Button onClick={fetchMatchedDestinations}>
+      <Button onClick={handleFetchClick}>
         {buttonText || 'Descobrir minha viagem'}
       </Button>
     </div>
