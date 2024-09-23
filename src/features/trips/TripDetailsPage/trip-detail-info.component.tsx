903f1da8586d83ee446fc34646f035491f48979c
Guilherme Azevedo <guilhermevba@gmail.com>
2024-09-22 21:51:07 -0300

    fix: build issues

--- src/features/trips/TripDetailsPage/trip-detail-info.component.tsx
+++ src/features/trips/TripDetailsPage/trip-detail-info.component.tsx
@@ -4,9 +4,10 @@ import { Grid } from "mars-ds";
 export interface TripDetailInfoPros {
   title: string;
   children?: React.ReactNode;
+  text?: string;
 }
 
-export const TripDetailInfo = ({ title, children }: TripDetailInfoPros) => {
+export const TripDetailInfo = ({ title, children, text }: TripDetailInfoPros) => {
   return (
     <Grid columns={["auto", "1fr"]} style={{ alignItems: "center" }}>
       <div style={{ backgroundColor: "#0ab9ad", width: 20, height: 20, borderRadius: "50%" }} />
@@ -14,6 +15,8 @@ export const TripDetailInfo = ({ title, children }: TripDetailInfoPros) => {
         <Text as="h3" size="xl" className="color-primary">
           {title}
         </Text>
+        {text ? <Text className="color-text-secondary">{text}</Text> : null}
+
         {children}
       </div>
     </Grid>
