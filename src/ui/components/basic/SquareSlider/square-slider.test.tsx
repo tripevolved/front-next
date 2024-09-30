4bbcba332d13947e29a08ae1802ed64ca4dd3e75
hmgasparotto <hmgasparotto@hotmail.com>
2024-09-30 18:33:29 -0300

    fix test

--- src/ui/components/basic/SquareSlider/square-slider.test.tsx
+++ src/ui/components/basic/SquareSlider/square-slider.test.tsx
@@ -1,10 +1,10 @@
-import type { CarouselProps } from "./square-slider.types";
+import type { SquareSliderProps } from "./square-slider.types";
 import { render } from "@testing-library/react";
-import { Carousel } from "./square-slider.component";
+import { SquareSlider } from "./square-slider.component";
 
-const makeSut = (props?: CarouselProps) => render(<Carousel {...props} />);
+const makeSut = (props?: SquareSliderProps) => render(<SquareSlider {...props} />);
 
-describe("<Carousel>", () => {
+describe("<SquareSlider>", () => {
   it.skip("should render component", () => {
     const wrapper = makeSut();
     expect(wrapper).toBeTruthy();
