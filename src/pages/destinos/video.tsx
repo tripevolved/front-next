d71fc3c39973d0e0aa240720b71e0fc5c399d097
hmgasparotto <hmgasparotto@hotmail.com>
2025-01-21 07:08:24 -0300

    remove test video from index to avoid test failure

--- src/pages/destinos/video.tsx
+++ src/pages/destinos/video.tsx
@@ -1,5 +1,5 @@
 import testVideo from '/videos/test.mp4';
-import { VerticalVideo } from '@/ui';
+import { VerticalVideo } from '@/ui/components/multimedia/VerticalVideo'; 
 
 export default function VideoPageRoute() {
   return <VerticalVideo src={testVideo} />
