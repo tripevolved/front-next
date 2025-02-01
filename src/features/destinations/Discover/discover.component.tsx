e152092d0d39e2a598db899e9269de81d9c0fbbb
hmgasparotto <hmgasparotto@hotmail.com>
2025-02-01 11:56:47 -0300

    adjust swipe behavior with swiper library

--- src/features/destinations/Discover/discover.component.tsx
+++ src/features/destinations/Discover/discover.component.tsx
@@ -1,49 +1,26 @@
-import { Box } from "@/ui";
 import type { DiscoverProps } from "./discover.types";
 import { VerticalVideo } from "@/ui/components/multimedia/VerticalVideo";
-import testVideo from '/videos/test_.mp4';
-import testHorizontal from "/videos/test-horizontal_.mp4";
-import { useRef, useState } from "react";
+import testVideo from '/videos/test.mp4';
+import testHorizontal from "/videos/test_horizontal.mp4";
+import testMov from "/videos/test2.mov";
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { } from 'swiper/modules';
 
 export function Discover({  }: DiscoverProps) {
-  const containerComponent = useRef<HTMLDivElement>(null);
-
-  const [touchStart, setTouchStart] = useState(null);
-  const [touchEnd, setTouchEnd] = useState(null);
-
-  // the required distance between touchStart and touchEnd to be detected as a swipe
-  const minSwipeDistance = 50;
-
-  const onTouchStart = (e: any) => {
-    setTouchEnd(null); // otherwise the swipe is fired even with usual touch events
-    setTouchStart(e.targetTouches[0].clientY);
-  }
-
-  const onTouchMove = (e: any) => setTouchEnd(e.targetTouches[0].clientY);
-
-  const onTouchEnd = () => {
-    if (!touchStart || !touchEnd) return;
-    const distance = touchStart - touchEnd;
-    const isUpSwipe = distance > minSwipeDistance;
-    const isDownSwipe = distance < -minSwipeDistance;
-    const height = containerComponent?.current?.clientHeight ?? 0;
-
-    if (isDownSwipe) {
-      containerComponent?.current?.scrollTo({ top: -height, behavior: "smooth" });
-    }
-    if (isUpSwipe) {
-      containerComponent?.current?.scrollTo({ top: height, behavior: "smooth" });
-    }
-  }
-
   return (
-    <div ref={containerComponent} className="discover" onTouchStart={onTouchStart} onTouchMove={onTouchMove} onTouchEnd={onTouchEnd}>
-      <div className="discover__item">
+    <Swiper
+      direction={"vertical"}
+      className="discover">
+      <SwiperSlide className="discover__item">
         <VerticalVideo src={testVideo} />
-      </div>
-      <div className="discover__item">
+      </SwiperSlide>
+      <SwiperSlide className="discover__item">
         <VerticalVideo src={testHorizontal} />
-      </div>
-    </div>
+      </SwiperSlide>
+      <SwiperSlide className="discover__item">
+        <VerticalVideo src={testMov} />
+      </SwiperSlide>
+    </Swiper>
   );
 }
