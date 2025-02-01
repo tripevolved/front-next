49f70e574c1f582c502f296c18949472583b1166
hmgasparotto <hmgasparotto@hotmail.com>
2025-02-01 16:44:16 -0300

    play img

--- src/ui/components/multimedia/VerticalVideo/vertical-video.component.tsx
+++ src/ui/components/multimedia/VerticalVideo/vertical-video.component.tsx
@@ -3,17 +3,19 @@ import Video from 'next-video';
 import { Box } from "@/ui";
 import { VerticalVideoProps } from "./vertical-video.types";
 import { makeCn } from "@/utils/helpers/css.helpers";
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import Image from 'next/image';
 
 export function VerticalVideo({ src, className, autoPlay, children, sx, ...props }: VerticalVideoProps) {
   const videoRef = useRef<HTMLVideoElement>(null);
+  const [showPlayImg, setShowPlayImg] = useState<Boolean>(false);
 
-  const playVideo = () => {
-    videoRef?.current?.play();
-  }
-
-  const pauseVideo = () => {
-    videoRef?.current?.pause();
+  const playPause = () => {
+    if (videoRef?.current?.paused) {
+      videoRef?.current?.play();
+    } else {
+      videoRef?.current?.pause();
+    }
   }
 
   const cn = makeCn("vertical-video", className)(sx);
@@ -27,12 +29,21 @@ export function VerticalVideo({ src, className, autoPlay, children, sx, ...props
           autoPlay={autoPlay ?? false}
           muted
           controls={false}
-          onClick={() => videoRef?.current?.paused ? playVideo() : pauseVideo()}
+          blurDataURL="/assets/videos/play.svg"
+          onClick={playPause}
+          onPlay={() => setShowPlayImg(false)}
+          onPause={() => setShowPlayImg(true)}
         >
-          {/* {(videoRef?.current?.paused && <img src={"playImage"}/>)} */}
           {children}
         </Video>
+        {showPlayImg && <PlayImage onClick={playPause} />}
       </Box>
     </Box>
+  );
+}
+
+function PlayImage({ onClick }: { onClick: () => void }) {
+  return (
+    <Image className="vertical-video__container__play" alt="play" width={64} height={64} src={"/assets/videos/play.svg"} onClick={onClick} />
   );
 }
