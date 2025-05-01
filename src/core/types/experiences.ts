096234869c67ab475fdee93ccf165e5053d23055
Henrique Machado Gasparotto <hmgasparotto@hotmail.com>
2025-05-01 15:56:16 -0300

    add images to califrornia

--- src/core/types/experiences.ts
+++ src/core/types/experiences.ts
@@ -190,22 +190,24 @@ export const mockExperiences: Experience[] = [
     dates: '24 a 31 de maio, 2024',
     travelers: 'por Henrique Gasparotto',
     description: 'A road trip para curtir o melhor das paisagens californianas, saindo de Los Angeles e chegando em San Francisco - mas de um jeito diferente.',
+    mapImage: '/assets/experiences/california/california-map.png',
     images: [
-      '/assets/experiences/northeast-1.jpg',
-      '/assets/experiences/northeast-2.jpg',
-      '/assets/experiences/northeast-3.jpg',
+      '/assets/experiences/california/la-griffith.png',
+      '/assets/experiences/california/lake-tahoe.png',
+      '/assets/experiences/california/san-francisco.png',
+      '/assets/experiences/california/yosemite.png',
     ],
     itinerary: [
       {
         day: 1,
         date: '24 de maio',
         activity: 'LA só de passagem',
-        image: '/assets/experiences/la-skyline.jpg',
+        image: '/assets/experiences/california/la-griffith.png',
         description: 'O dia em Los Angeles foi só de passagem, mas com uma chegada no Observatório Griffith antes de partir rumo a Morro Bay.',
         hotel: {
           name: 'Holland Inn & Suites',
           description: 'Um hotel simples e funcional em Morro Bay, com fácil acesso às principais atrações da cidade.',
-          image: '/assets/hotels/holland-inn.jpg'
+          image: '/assets/experiences/california/holland-inn.jpg'
         },
         highlights: {
           description: 'A viagem era o atrativo do dia: o Observatório Griffith para começar. Depois, Highway 1, Topanga State Park e paisagens incríveis pela Los Padres National Forest.',
@@ -216,12 +218,12 @@ export const mockExperiences: Experience[] = [
         day: 2,
         date: '25 de maio',
         activity: 'Uma manhã única em Morro Bay e partiu rumo Fresno',
-        image: '/assets/experiences/santa-barbara.jpg',
+        image: '/assets/experiences/california/morro-rock.png',
         description: 'A manhã em Morro Bay foi incrível. A cidadezinha é muito tranquila, com a vista para a Morro Rock sendo um show à parte.',
         hotel: {
-          name: 'Hotel Picadilly',
+          name: 'Hotel Piccadilly',
           description: 'Um hotel charmoso, bem localizado, em Fresno, com fácil acesso à rodovia e aos parques nacionais.',
-          image: '/assets/hotels/picadilly.jpg'
+          image: '/assets/experiences/california/hotel-piccadilly.jpg'
         },
         highlights: {
           description: 'Morro Bay. Uma parada um tanto aleatória na nossa viagem, mas foi muito legal conhecer um pouco mais do estilo por lá. Completamente diferente de tudo que vemos nos filmes, que normalmente se passam em LA ou San Francisco.',
@@ -232,12 +234,12 @@ export const mockExperiences: Experience[] = [
         day: 3,
         date: '26 de maio',
         activity: 'Sequoia & Kings Canyon - Que lugar! E para fechar, Yosemite no fim da tarde',
-        image: '/assets/experiences/big-sur.jpg',
+        image: '/assets/experiences/california/sequoia.png',
         description: 'Fresno é a porta de entrada para alguns dos parques nacionais mais impressionantes dos EUA: Sequoia & Kings Canyon e o mundialmente famoso Yosemite National Park. Os lugares valem cada segundo da visita.',
         hotel: {
           name: 'Fairfield Inn & Suites Oakhurst Yosemite',
           description: 'Um hotel confortável e moderno, localizado estrategicamente para explorar o Yosemite National Park, com piscina aquecida e café da manhã incluso.',
-          image: '/assets/hotels/fairfield-oakhurst.jpg'
+          image: '/assets/experiences/california/hotel-fairfield.jpg'
         },
         highlights: {
           description: 'A manhã começou por visitar as Sequoias e elas fazem você se sentir pequeno. Natureza pura! Mas o ponto alto do alto foi a vista de El Capitan no fim do dia, já em Yosemite. Sensacional!',
@@ -248,12 +250,12 @@ export const mockExperiences: Experience[] = [
         day: 4,
         date: '27 de maio',
         activity: 'Yosemite e ponto final',
-        image: '/assets/experiences/monterey.jpg',
+        image: '/assets/experiences/california/yosemite-2.png',
         description: 'Sabíamos que Yosemite merecia mais do que uma visita rápida de fim de dia e não nos decepcionamos. É um espetáculo atrás do outro.',
         hotel: {
           name: 'Fairfield Inn & Suites Oakhurst Yosemite',
           description: 'Um hotel confortável e moderno, localizado estrategicamente para explorar o Yosemite National Park, com piscina aquecida e café da manhã incluso.',
-          image: '/assets/hotels/fairfield-oakhurst.jpg'
+          image: '/assets/experiences/california/hotel-fairfield.jpg'
         },
         highlights: {
           description: 'Queríamos uma trilha tranquila - mas erramos a curva e não foi tão tranquilo. A compensação foi chegarmos ao topo da Vernal Fall e ao Clark Point: são realmente espetaculares. Depois, descobrimos que pegamos a John Muir Trail.',
@@ -264,12 +266,12 @@ export const mockExperiences: Experience[] = [
         day: 5,
         date: '28 de maio',
         activity: 'Lake Tahoe - (quase) ainda na Califórnia!',
-        image: '/assets/experiences/napa-valley.jpg',
+        image: '/assets/experiences/california/lake-tahoe.png',
         description: 'Um pouco mais ao norte, Lake Tahoe é uma das maravilhas naturais da Califórnia. Partimos cedo de Oakhurst, passamos pela Eldorado National Forest e chegamos em South Lake Tahoe, nossa base para conhecer esse lugar lindo.',
         hotel: {
           name: 'Station House Inn',
           description: 'Um hotel tranquilo em South Lake Tahoe, com fácil acesso às praias e atrações do lago.',
-          image: '/assets/hotels/station-house.jpg'
+          image: '/assets/experiences/california/station-house-inn.jpg'
         },
         highlights: {
           description: 'Passar ao lado de Nevada e achar um cantinho lindo, cheio de pedras, para só curtir a vista do lago.',
@@ -280,12 +282,12 @@ export const mockExperiences: Experience[] = [
         day: 6,
         date: '29 de maio',
         activity: 'Dia de praia em Tahoe!',
-        image: '/assets/experiences/sonoma.jpg',
+        image: '/assets/experiences/california/lake-tahoe-2.png',
         description: 'Num cantinho pertinho de Kings Beach, descobrimos uma praia tranquila para relaxar e curtir o dia. O que dizer da água: gelada, muito gelada! Mas tínhamos que mergulhar um pouco, certo?',
         hotel: {
           name: 'Station House Inn',
           description: 'Um hotel tranquilo em South Lake Tahoe, com fácil acesso às praias e atrações do lago.',
-          image: '/assets/hotels/station-house.jpg'
+          image: '/assets/experiences/california/station-house-inn.jpg'
         },
         highlights: {
           description: 'Ponto alto também para Emerald Bay, um cantinho do lago com uma vista espetacular para uma ilhota, que cria um cenário lindo.',
@@ -296,12 +298,12 @@ export const mockExperiences: Experience[] = [
         day: 7,
         date: '30 de maio',
         activity: 'São Francisco - A cidade da baía',
-        image: '/assets/experiences/san-francisco.jpg',
+        image: '/assets/experiences/california/san-francisco.png',
         description: 'A chegada em São Francisco tinha que ser pela Golden Gate Bridge, certo? Fizemos questão que nosso caminho nos trouxesse por lá.',
         hotel: {
           name: 'The Donatello',
           description: 'Um hotel elegante no centro de São Francisco, próximo à Union Square, com quartos espaçosos e vista para a cidade. O ponto alto é o lounge, no topo do hotel.',
-          image: '/assets/hotels/donatello.jpg'
+          image: '/assets/experiences/california/the-donatello.jpg'
         },
         highlights: {
           description: 'Estávamos muito bem localizados, pertinho de Union Square e dos principais pontos. O Ferry Building foi o ponto alto do dia, com direito a empanada argentina!',
@@ -312,12 +314,12 @@ export const mockExperiences: Experience[] = [
         day: 8,
         date: '31 de maio',
         activity: 'Piers, muitos piers. E, claro, Alcatraz!',
-        image: '/assets/experiences/sf-streets.jpg',
+        image: '/assets/experiences/california/alcatraz.png',
         description: 'São Francisco oferece muitas vistas da Baía, e tínhamos que aproveitar. Coit Tower, Palace of Fine Arts... Mas, principalmente, os piers em torno do Fisherman\'s Wharf.',
         hotel: {
           name: 'The Donatello',
           description: 'Um hotel elegante no centro de São Francisco, próximo à Union Square, com quartos espaçosos e vista para a cidade. O ponto alto é o lounge, no topo do hotel.',
-          image: '/assets/hotels/donatello.jpg'
+          image: '/assets/experiences/california/the-donatello.jpg'
         },
         highlights: {
           description: 'A viagem para Alcatraz foi uma das mais legais de toda a viagem. A experiência toda, com as histórias contadas lá dentro, vale muito a pena. E as vistas do trajeto de barco...',
