a58a4cb687c3559bade0bc7853f5a9b167dad77a
juangarciadelima <juangarciadelima884@gmail.com>
2025-05-01 16:14:42 -0300

    fix: removing unused code

--- src/features/trips/TripDetailsPage/trip-details-page.component.tsx
+++ src/features/trips/TripDetailsPage/trip-details-page.component.tsx
@@ -139,7 +139,6 @@ export function TripDetailsPage() {
                   username,
                   email,
                 }}
-                configuration={configuration}
               />
             </Grid>
           </div>
