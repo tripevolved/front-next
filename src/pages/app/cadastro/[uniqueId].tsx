310f80b1e7a13a5a5114c4bf9b9e169d463a067a
Henrique Machado Gasparotto <hmgasparotto@hotmail.com>
2025-12-16 12:08:35 -0300

    fix build

--- src/pages/app/cadastro/[uniqueId].tsx
+++
@@ -1,9 +0,0 @@
-import {  AuthPasswordConfirmation, PageApp } from "@/features";
-
-export default function SignUpConfirmationRoute() {
-  return (
-    <PageApp hideHeader seo={{ title: "Finalizar cadastro" }}>
-      <AuthPasswordConfirmation />
-    </PageApp>
-  );
-}
