22142984d328e8fd4a715bb8df8ad573a278d59e
Henrique Gasparotto <hmgasparotto@hotmail.com>
2026-06-26 14:28:07 -0400

    remove unused endpoint

--- src/services/api/trip/index.ts
+++ src/services/api/trip/index.ts
@@ -1,8 +1,6 @@
 import { getTripDestinationQuestions, getTripQuestions } from "./questions";
-import { removeById } from "./remove";
 
 export const TripsApiService = {
   getTripDestinationQuestions: getTripDestinationQuestions,
   getTripQuestions: getTripQuestions,
-  removeById,
 };
--- src/services/api/trip/remove.ts
+++
@@ -1,6 +0,0 @@
-import { ApiRequest } from "../request";
-
-export const removeById = async (tripId: string) => {
-  const route = `/trips/${tripId}/cancel`;
-  await ApiRequest.delete(route);
-};
